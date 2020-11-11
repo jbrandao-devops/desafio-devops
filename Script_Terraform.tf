856dfc50e42184d02edcad74464e1887c710d2ae
Jorge Brandao Junior <jorge.brandao@bratech.net>
2020-11-11 19:08:51 +0000

    Enviar Novo Arquivo

---
+++ Script_Terraform.tf
@@ -0,0 +1,30 @@
+# Cria instacia principal WordPress Certified by Bitnami and Automattic
+# c5.xlarge node with an AWS Tag naming it "Inst-001"
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "c5.xlarge"
+
+  tags = {
+    Name = "Inst-001"
+  }
+}
